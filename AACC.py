23e462e60491dc30cb9c879d42c29f1b9692fe3a
Haakon <haakot@stud.ntnu.no>
2019-03-28 19:11:24 +0100

    Starting point

---
+++ AACC.py
@@ -0,0 +1,12 @@
+#
+# Automatic Atomic Column Characterizer (AACC).
+# ----------------------------------------
+# By Haakon Tvedt.
+# ----------------------------------------
+# Master project in technical physics at NTNU. Supervised by Prof. Randi Holmestad. Co-supervisors Calin Maroiara and
+# Jesper Friis at SINTEF.
+# ----------------------------------------
+# This file is the entry point of the software.
+# ----------------------------------------
+#
+
