16b6479f71cb4064e067926e97100c89ed81beb4
Haakon <haakot@stud.ntnu.no>
2019-03-28 21:11:39 +0100

    fix file naming

--- mat_op.py
+++ mat_op.py
@@ -2,7 +2,7 @@
 # -------------------------------------------
 # This script contains typical functions that manipulate matrices with inputs and outputs that are logically
 # connected to the main program. All inputs "mat" (short for matrix) are assumed to be 2d matrices with float64
-# elements normalized to be in the range [0, 1].
+# elements normalized to be in the range [0, 1]. Hello
 
 import numpy as np
 from PIL import Image
