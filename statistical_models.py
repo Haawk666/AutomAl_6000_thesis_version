b464cb66aacacd282298003eefa34079588e0653
Haawk <haakot@stud.ntnu.no>
2020-04-04 20:50:15 +0200

    Continued integration of new graph module

---
+++ statistical_models.py
@@ -0,0 +1,56 @@
+"""This module contains the different statistical models used in the column characterization algorithm. It also has
+methods that can gather model parameters from sets of overlay files. The default model parameters used here are
+gathered from the images in the validation data set. See automal.org for more details."""
+
+
+def alpha_model(alpha_angles):
+    pass
+
+
+def theta_model(Theta_angles):
+    pass
+
+
+def normalized_gamma_model(normalized_peak_gamma, normalized_avg_gamma):
+    pass
+
+
+def composite_model(alpha_angles, theta_angles, normalized_peak_gamma, normalized_avg_gamma):
+    pass
+
+
+def product_model(alpha_angles, theta_angles, normalized_peak_gamma, normalized_avg_gamma):
+    pass
+
+
+def weighted_model():
+    pass
+
+
+def get_model_parameters(model):
+    """Get the parameters for the particular model."""
+
+    if model == 0:
+        pass
+
+    elif model == 1:
+        pass
+
+    elif model == 2:
+        pass
+
+    elif model == 3:
+        pass
+
+    elif model == 4:
+        pass
+
+    elif model == 5:
+        pass
+
+    elif model == 6:
+        pass
+
+    else:
+        return None
+
